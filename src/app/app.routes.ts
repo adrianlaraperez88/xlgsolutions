d480c2c1f30f41a6c97ab082a04499ceaa2b8f07
Adrian Lazaro Lara Perez <adrianlaraperez88@gmail.com>
2026-07-15 17:26:57 -0400

    feat: initialize application routing configuration with lazy-loaded modules and default path redirects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,35 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ServiceDetailComponent } from './pages/service-detail/service-detail.component';
-import { ResourcesComponent } from './pages/resources/resources.component';
-import { ResourceDetailComponent } from './pages/resource-detail/resource-detail.component';
-import { ToolsComponent } from './pages/tools/tools.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'XLG Solutions - Home' },
-    { path: 'service/:id', component: ServiceDetailComponent, title: 'XLG Solutions - Service Details' },
-    { path: 'resources', component: ResourcesComponent, title: 'XLG Solutions - Educational Guides & Resources' },
-    { path: 'resources/:id', component: ResourceDetailComponent, title: 'XLG Solutions - Resource' },
-    { path: 'tools', component: ToolsComponent, title: 'XLG Solutions - Interactive Tax & ACA Tools' },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { 
+      path: '', 
+      component: HomeComponent, 
+      title: 'XLG Solutions - Home' 
+    },
+    { 
+      path: 'service/:id', 
+      loadComponent: () => import('./pages/service-detail/service-detail.component').then(m => m.ServiceDetailComponent), 
+      title: 'XLG Solutions - Service Details' 
+    },
+    { 
+      path: 'resources', 
+      loadComponent: () => import('./pages/resources/resources.component').then(m => m.ResourcesComponent), 
+      title: 'XLG Solutions - Educational Guides & Resources' 
+    },
+    { 
+      path: 'resources/:id', 
+      loadComponent: () => import('./pages/resource-detail/resource-detail.component').then(m => m.ResourceDetailComponent), 
+      title: 'XLG Solutions - Resource' 
+    },
+    { 
+      path: 'tools', 
+      loadComponent: () => import('./pages/tools/tools.component').then(m => m.ToolsComponent), 
+      title: 'XLG Solutions - Interactive Tax & ACA Tools' 
+    },
+    { 
+      path: '**', 
+      redirectTo: '', 
+      pathMatch: 'full' 
+    }
 ];
